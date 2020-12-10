ced8db850d4b6fbde5dcd4801462f1095a88a453
jcoliver <jcoliver@users.noreply.github.com>
2020-12-10 19:16:58 +0000

    Deploy to GitHub Pages

---
+++ bin/test_lesson_check.py
@@ -0,0 +1,19 @@
+import unittest
+
+import lesson_check
+import util
+
+
+class TestFileList(unittest.TestCase):
+    def setUp(self):
+        self.reporter = util.Reporter()  # TODO: refactor reporter class.
+
+    def test_file_list_has_expected_entries(self):
+        # For first pass, simply assume that all required files are present
+
+        lesson_check.check_fileset('', self.reporter, lesson_check.REQUIRED_FILES)
+        self.assertEqual(len(self.reporter.messages), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
